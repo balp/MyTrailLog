29a50f8e8b533975e6467531b3167e68a3ca9f7b
Anders Arnholm <anders.arnholm@hiq.se>
2024-10-28 07:09:11 +0100

    Initial vue app

---
+++ mytraillog/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <TripLog />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import TripLog from "@/components/TripLog.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TripLog
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ mytraillog/src/components/TripLog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="triplog">
+    <h1>Logs</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import {ref} from 'vue'
+
+  var tripData = ref(null)
+
+  async function fetchTrips() {
+    tripData.value = null
+    const res = await fetch(
+        'http://localhost:8081/api/v1/trips'
+    )
+    tripData.value = await res.json()
+  }
+
+  fetchTrips()
+</script>
+
+<style scoped>
+
+</style>
