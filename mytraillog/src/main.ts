efd7ebec38e363e276b01fef401db3563d201b09
Anders Arnholm <anders.arnholm@hiq.se>
2024-10-30 10:36:56 +0100

    Split front and backend

--- mytraillog/src/main.ts
+++ mytraillog/src/main.ts
@@ -4,8 +4,8 @@ import router from './router'
 import { initializeApp } from 'firebase/app'
 
 const config = {
-    apiKey: "",
-    authDomain: "",
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
 };
 export const firebase_app = initializeApp(config);
 createApp(App).use(router).mount('#app')
